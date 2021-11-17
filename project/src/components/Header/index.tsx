442495381bc4849afe6837289501a37a589023cd
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-11-17 12:22:48 -0300

    Adding img

--- project/src/components/Header/index.tsx
+++ project/src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
 import { Container, Nav } from './styles';
 
+import React from '../../assets/react.png'
+
 type Props = {
   children: ReactNode;
 }
@@ -8,7 +10,7 @@ type Props = {
 export const Header = ({ children }: Props) => {
   return (
     <Container>
-      <img src={"https://cdn-icons.flaticon.com/png/512/520/premium/520890.png?token=exp=1637161006~hmac=0169f19b616fd3aab8cf47e97edd4738"} />
+      <img src={React} />
       <Nav>
         {children}
       </Nav>
